dc48bd36d72ec8994db86bd0164add91b62ce584
Mikhail Adamenko <michailadamenko@gmail.com>
2015-10-29 03:28:24 +0600

    0.8
    pre-release version

---
+++ js/index.js
@@ -0,0 +1,8 @@
+////////////////////////////////
+// Navbar one page navigation
+////////////////////////////////
+$('.navbar').onePageNav({
+		currentClass: 'active',
+		changeHash: true,
+		scrollSpeed: 1200
+});
